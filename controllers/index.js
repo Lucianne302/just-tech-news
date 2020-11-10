f2a5c51dbf1692c28c275cc7bcbfd893bff380d3
lucianne302 <lucianne21@comcast.net>
2020-11-10 14:20:41 -0500

    created dashboard, edit and delete.  End of module 14.5.6

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,9 @@ const userRoutes = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
